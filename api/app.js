cc127d6f5b6f37ce3340b8adfffbc6c05107524b
patjagielski <jagielskipat@gmail.com>
2021-02-22 15:27:34 +0100

    endpoints for functionalities complete

--- api/app.js
+++ api/app.js
@@ -23,15 +23,45 @@ var connection = mysql.createConnection({
   
   app.get("/getNotes", (req, res)=>{
     connection.query("SELECT * FROM notes", (err, rows)=>{
-      if(err){
+      if(err)
         throw err;
-      }else{
+      else
         res.send(rows);
-      }
+      
     });
 
   })
 
+  app.post("/addNotes", (req, res)=>{
+    connection.query("INSERT INTO	Notes( version, title, date_created, last_modified, content) VALUES ( 0, 'NOTE', NOW(), NOW(), 'Note content goes here')",(err, rows)=>{
+      if(err)
+        throw err;
+      else
+        res.send(rows);
+    })
+  })
+
+  app.post("/removeNote", (req, res)=>{
+    const id = req.body.idnotes;
+    connection.query("DELETE FROM Notes where idnotes = ?", [id], (err, rows)=>{
+      if(err) throw err
+      else res.send(rows);
+    })
+  })
+
+  app.patch("/editNote", (req, res)=>{
+    const id =req.body.idnotes;
+    const version = req.body.version;
+    const title = req.body.title;
+    const content = req.body.content;
+    const last_modified = req.body.last_modified;
+    console.log(id)
+
+    connection.query("UPDATE notes SET version=?, title=?, content=?, last_modified=? WHERE idnotes=?", [version,title,content, last_modified, id], (err, rows)=>{
+      if(err) throw err
+      else res.send(rows);
+    })
+  })
 
 const port = process.env.PORT || 5000;
 app.listen(port);
