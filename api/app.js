f0aafa1130fdea20c6711d6f00de62aa722b0f1d
patjagielski <jagielskipat@gmail.com>
2021-02-23 22:55:45 +0100

    small changes

--- api/app.js
+++ api/app.js
@@ -39,7 +39,7 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-// console.log("app is listening to " + port)
+console.log("app is listening to " + port)
 module.exports = app
 
 
